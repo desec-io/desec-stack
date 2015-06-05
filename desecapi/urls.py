60b333a4ea50159878f87cf8a3bd48070e724442
Nils Wisiol <info@nils-wisiol.de>
2015-06-05 18:16:59 +0200

    fix(urls): allow domain names with 0-9 in it

--- desecapi/urls.py
+++ desecapi/urls.py
@@ -6,7 +6,7 @@ from rest_framework.urlpatterns import format_suffix_patterns
 apiurls = [
     url(r'^$', Root.as_view(), name='root'),
     url(r'^domains/$', DomainList.as_view(), name='domain-list'),
-    url(r'^domains/(?P<name>[a-zA-Z\.\-]+)/$', DomainDetailByName.as_view(), name='domain-detail'),
+    url(r'^domains/(?P<name>[a-zA-Z\.\-0-9]+)/$', DomainDetailByName.as_view(), name='domain-detail'),
     url(r'^domains/(?P<pk>[0-9]+)/$', DomainDetail.as_view(), name='domain-detail'),
     url(r'^dns$', DnsQuery.as_view(), name='dns-query'),
     url(r'^scan/logjam$', ScanLogjam.as_view(), name='scan-logjam'),
