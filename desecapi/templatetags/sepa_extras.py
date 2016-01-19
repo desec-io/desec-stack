1c0eaf46fc1884535a8ad7ecf3b2b565d41afb4f
Nils Wisiol <info@nils-wisiol.de>
2016-01-19 11:46:35 +0100

    fix(docs): fixed code documentation

--- desecapi/templatetags/sepa_extras.py
+++ desecapi/templatetags/sepa_extras.py
@@ -6,7 +6,7 @@ register = template.Library()
 
 def clean(value):
     """Replaces non-ascii characters with their closest ascii
-       representation and then removes everything but [A-Za-z0-9]"""
+       representation and then removes everything but [A-Za-z0-9 ]"""
     normalized = unicodedata.normalize('NFKD', value)
     cleaned = re.sub(r'[^A-Za-z0-9 ]','',normalized)
     return cleaned
