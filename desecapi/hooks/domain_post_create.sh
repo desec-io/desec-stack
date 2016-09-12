cd787f771e9d8ad3d551b6e5e7ee43dca3f83298
Peter Thomassen <peter@desec.io>
2016-09-12 20:33:33 -0300

    fix(hooks): small debugging fix

--- desecapi/hooks/domain_post_create.sh
+++ desecapi/hooks/domain_post_create.sh
@@ -13,7 +13,6 @@ SALT=`head -c300 /dev/urandom | sha512sum | cut -b 1-16`
 filename=/tmp/`date -Ins`_$ZONE.log
 touch $filename
 chmod 640 $filename
-echo $DATA > $filename
 
 echo "sign, post-update $ZONE"
 pdnsutil secure-zone $ZONE; pdnsutil set-nsec3 $ZONE "1 0 10 $SALT" && `dirname $0`/domain_post_update.sh $ZONE || exit 2
@@ -23,6 +22,7 @@ DATA='{"rrsets": [ {"name": "'"$ZONE".'", "type": "DS", "ttl": 60, "changetype":
 DATA+=`curl -sS -X GET -H "X-API-Key: $APITOKEN" http://127.0.0.1:8081/api/v1/servers/localhost/zones/$ZONE/cryptokeys \
 	| jq -c '[.[] | select(.active == true) | {content: .ds[]?, disabled: false}]'`
 DATA+=" } ] }"
+echo $DATA >> $filename
 
 echo "Setting DS records in parent zone $PARENT"
 curl -sSv -X PATCH --data "$DATA" -H "X-API-Key: $APITOKEN" http://127.0.0.1:8081/api/v1/servers/localhost/zones/$PARENT &>> $filename || exit 3
