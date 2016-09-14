f674b5e704e5e86b832c5390375d4f8ff7fa8c42
Peter Thomassen <peter@desec.io>
2016-09-14 20:13:43 -0300

    fix(hooks): revert back to notification via pdns_control since we've reduced the number of updates

--- desecapi/hooks/domain_post_update.sh
+++ desecapi/hooks/domain_post_update.sh
@@ -13,9 +13,9 @@ ZONE=$1
 pdnsutil rectify-zone $ZONE
 pdnsutil increase-serial $ZONE
 
-#pdns_control notify $ZONE
-dig -b 178.63.189.78 +short +opcode=NOTIFY SOA $ZONE @54.88.76.245
-dig -b 178.63.189.78 +short +opcode=NOTIFY SOA $ZONE @178.63.189.72
+pdns_control notify $ZONE
+#dig -b 178.63.189.78 +short +opcode=NOTIFY SOA $ZONE @54.88.76.245
+#dig -b 178.63.189.78 +short +opcode=NOTIFY SOA $ZONE @178.63.189.72
 
 echo -n "This was $0: "
 date
