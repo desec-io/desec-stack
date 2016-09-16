2481aabee2090ad4bc116bbb206e8f103c21d38f
Peter Thomassen <peter@desec.io>
2016-09-15 22:52:16 -0300

    feat(hooks): remove increase-serial and notify 1.) increase-serial is done automatically when updating records through the pdns REST API 2.) notify is done automatically when serial has changed, see `slave-cycle-interval` setting

--- desecapi/hooks/domain_post_update.sh
+++ desecapi/hooks/domain_post_update.sh
@@ -11,11 +11,6 @@ set -ex
 ZONE=$1
 
 pdnsutil rectify-zone $ZONE
-pdnsutil increase-serial $ZONE
-
-pdns_control notify $ZONE
-#dig -b 178.63.189.78 +short +opcode=NOTIFY SOA $ZONE @54.88.76.245
-#dig -b 178.63.189.78 +short +opcode=NOTIFY SOA $ZONE @178.63.189.72
 
 echo -n "This was $0: "
 date
