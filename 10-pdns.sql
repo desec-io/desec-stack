5e6ceeb56d75c83e02b91f088a14a93d4caaa7c5
Peter Thomassen <peter@desec.io>
2016-09-19 11:50:33 -0300

    fix(pdns): require latin1 charset explicitly as recent MariaDB releases use multibyte charsets, breaking index creation due to length restrictions

--- 10-pdns.sql
+++ 10-pdns.sql
@@ -9,7 +9,7 @@ CREATE TABLE domains (
   notified_serial       INT DEFAULT NULL,
   account               VARCHAR(40) DEFAULT NULL,
   PRIMARY KEY (id)
-) Engine=InnoDB;
+) Engine=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE UNIQUE INDEX name_index ON domains(name);
 
@@ -27,7 +27,7 @@ CREATE TABLE records (
   ordername             VARCHAR(255) BINARY DEFAULT NULL,
   auth                  TINYINT(1) DEFAULT 1,
   PRIMARY KEY (id)
-) Engine=InnoDB;
+) Engine=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE INDEX nametype_index ON records(name,type);
 CREATE INDEX domain_id ON records(domain_id);
@@ -39,7 +39,7 @@ CREATE TABLE supermasters (
   nameserver            VARCHAR(255) NOT NULL,
   account               VARCHAR(40) NOT NULL,
   PRIMARY KEY (ip, nameserver)
-) Engine=InnoDB;
+) Engine=InnoDB DEFAULT CHARSET=latin1;
 
 
 CREATE TABLE comments (
@@ -51,7 +51,7 @@ CREATE TABLE comments (
   account               VARCHAR(40) NOT NULL,
   comment               VARCHAR(64000) NOT NULL,
   PRIMARY KEY (id)
-) Engine=InnoDB;
+) Engine=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE INDEX comments_domain_id_idx ON comments (domain_id);
 CREATE INDEX comments_name_type_idx ON comments (name, type);
@@ -64,7 +64,7 @@ CREATE TABLE domainmetadata (
   kind                  VARCHAR(32),
   content               TEXT,
   PRIMARY KEY (id)
-) Engine=InnoDB;
+) Engine=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE INDEX domainmetadata_idx ON domainmetadata (domain_id, kind);
 
@@ -76,7 +76,7 @@ CREATE TABLE cryptokeys (
   active                BOOL,
   content               TEXT,
   PRIMARY KEY(id)
-) Engine=InnoDB;
+) Engine=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE INDEX domainidindex ON cryptokeys(domain_id);
 
@@ -87,7 +87,7 @@ CREATE TABLE tsigkeys (
   algorithm             VARCHAR(50),
   secret                VARCHAR(255),
   PRIMARY KEY (id)
-) Engine=InnoDB;
+) Engine=InnoDB DEFAULT CHARSET=latin1;
 
 CREATE UNIQUE INDEX namealgoindex ON tsigkeys(name, algorithm);
 
