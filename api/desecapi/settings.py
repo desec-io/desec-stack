b258c36bdc38014772763faadcc60ba1650a9723
Nils Wisiol <info@nils-wisiol.de>
2016-11-22 11:02:52 +0100

    fix(django): add template settings

--- api/desecapi/settings.py
+++ api/desecapi/settings.py
@@ -117,6 +117,22 @@ DJOSER = {
     'SEND_ACTIVATION_EMAIL': False,
 }
 
+TEMPLATES = [
+    {
+        'BACKEND': 'django.template.backends.django.DjangoTemplates',
+        'DIRS': [],
+        'APP_DIRS': True,
+        'OPTIONS': {
+            'context_processors': [
+                'django.template.context_processors.debug',
+                'django.template.context_processors.request',
+                'django.contrib.auth.context_processors.auth',
+                'django.contrib.messages.context_processors.messages',
+            ],
+        },
+    },
+]
+
 # How to send mail
 EMAIL_HOST = ''
 EMAIL_HOST_USER = ''
