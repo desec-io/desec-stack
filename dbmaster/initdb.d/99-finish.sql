504f21de2622dea26b696d73775c9e8f84ea6346
Nils Wisiol <info@nils-wisiol.de>
2017-06-22 17:42:20 +0200

    fix(db): init command violated unique constraint
    There are four users in the database:
    * generic root,
    * local root
    * desec
    * desec_test
    
    Hence, the old update command failed as it tried to update both root
    users to be 'localhost'.

--- dbmaster/initdb.d/99-finish.sql
+++ dbmaster/initdb.d/99-finish.sql
@@ -1,4 +1,4 @@
 -- Narrow down root logins
 INSTALL PLUGIN unix_socket SONAME 'auth_socket';
-UPDATE mysql.user SET Host = 'localhost', plugin = 'unix_socket' WHERE User = 'root';
+UPDATE mysql.user SET plugin = 'unix_socket' WHERE User = 'root' AND Host = 'localhost';
 FLUSH PRIVILEGES;
