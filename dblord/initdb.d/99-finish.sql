256c0b60b3fe19b09be9bd7914b1b0e8112f4015
Nils Wisiol <info@nils-wisiol.de>
2017-06-26 15:34:05 +0200

    fix(db): remove unnecessary non-localhost root user

--- dblord/initdb.d/99-finish.sql
+++ dblord/initdb.d/99-finish.sql
@@ -1,4 +1,5 @@
 -- Narrow down root logins
 INSTALL PLUGIN unix_socket SONAME 'auth_socket';
+DELETE FROM mysql.user WHERE User = 'root' AND Host != 'localhost';
 UPDATE mysql.user SET plugin = 'unix_socket' WHERE User = 'root' AND Host = 'localhost';
 FLUSH PRIVILEGES;
