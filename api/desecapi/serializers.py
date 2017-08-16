592da98d80027d6332ea4c4d6c1799af8327b49c
Nils Wisiol <info@nils-wisiol.de>
2017-08-16 14:12:47 +0200

    fix(api): remove timestamps from domain serialization
    Closes #55

--- api/desecapi/serializers.py
+++ api/desecapi/serializers.py
@@ -48,8 +48,7 @@ class DomainSerializer(serializers.ModelSerializer):
 
     class Meta:
         model = Domain
-        fields = ('name', 'owner', 'arecord', 'aaaarecord', 'created', 'updated', 'acme_challenge', 'keys')
-        read_only_fields = ('created', 'updated',)
+        fields = ('name', 'owner', 'arecord', 'aaaarecord', 'acme_challenge', 'keys')
 
 
 class DonationSerializer(serializers.ModelSerializer):
