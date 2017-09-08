80fbd96ab691e763b93e0acda38bbadae5509519
Nils Wisiol <info@nils-wisiol.de>
2017-09-08 20:56:40 +0200

    chore(www): move www to desec-stack repository
    This makes the desec-www repository obsolete. Files takes from
    c014031ae7ab45ee93f1c86a1185f9fad710918d.

---
+++ www/conf/sites-available/envreplace.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+# https://stackoverflow.com/questions/59895/can-a-bash-script-tell-which-directory-it-is-stored-in
+DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
+
+for file in $DIR/*.conf.var; do
+    # we only replace occurances of the variables specified below as first argument
+    envsubst '$DESECSTACK_DOMAIN' < $file > $DIR/`basename $file .var`
+done
