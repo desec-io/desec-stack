4bdee23afd5542be45c46fd70127c03683f4f14e
Nils Wisiol <info@nils-wisiol.de>
2018-04-15 19:28:01 +0800

    feat(webapp): add development version of vuejs based webapp
    Notes:
    1. In this version, cd webapp && npm install is required to run to the
       development machine. The docker container just ships the node binary,
       but uses the entire webapp/ directory as volume.
    2. That's why live reload after code changes is supported -- very
       convenient for development.
    3. No production version included. We still need to figure that part out.
    
    Try https://${DESECSTACK_DOMAIN}/webapp/ !

--- www/conf/sites-available/envreplace.sh
+++ www/conf/sites-available/envreplace.sh
@@ -5,5 +5,10 @@ DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
 for file in $DIR/*.conf.var; do
     # we only replace occurances of the variables specified below as first argument
-    (envsubst '$DESECSTACK_DOMAIN' | envsubst '$CERT_PATH') < $file > $DIR/`basename $file .var`
+    (
+		envsubst '$DESECSTACK_DOMAIN' \
+		| envsubst '$CERT_PATH' \
+		| envsubst '$DESECSTACK_API_DEV' \
+		| envsubst '$DESECSTACK_API_PROD' \
+    ) < $file > $DIR/`basename $file .var`
 done
