a9b6bc827dcde8ea55dc2b3d292f971f4b558ef5
Nils Wisiol <info@nils-wisiol.de>
2018-08-06 16:16:49 +0200

    added missing file

---
+++ webapp/src/components/LogIn.vue
@@ -0,0 +1,83 @@
+<template>
+  <v-card flat>
+    <v-form v-model="valid" @submit="login()">
+      <v-toolbar flat>
+        <v-toolbar-title>Log in</v-toolbar-title>
+      </v-toolbar>
+      <v-card-text>
+        <v-alert :value="errors && errors.length" type="error">
+          <div v-if="errors.length > 1">
+            <li v-for="error of errors" :key="error.message">
+              <b>{{ error.message }}</b>
+              {{ error }}
+            </li>
+          </div>
+          <div v-else>
+            {{ errors[0] }}
+          </div>
+        </v-alert>
+          <v-text-field
+            v-model="email"
+            :rules="email_rules"
+            label="E-mail"
+            required
+          ></v-text-field>
+          <v-text-field
+            v-model="password"
+            :append-icon="hide_password ? 'visibility' : 'visibility_off'"
+            @click:append="() => (hide_password = !hide_password)"
+            :type="hide_password ? 'password' : 'text'"
+            label="Enter your password"
+            counter
+            required
+          ></v-text-field>
+      </v-card-text>
+      <v-card-actions>
+        <v-btn type="submit" color="primary" :disabled="!valid" id="login-button">Log in</v-btn>
+      </v-card-actions>
+    </v-form>
+  </v-card>
+</template>
+
+<script>
+import {HTTP} from '../http-common'
+
+export default {
+  name: 'LogIn',
+  data: () => ({
+    ack: false,
+    email: '',
+    email_rules: [
+      v => !!v || 'E-mail is required',
+      v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
+    ],
+    hide_password: true,
+    password: '',
+    valid: false,
+    working: false,
+    errors: []
+  }),
+  methods: {
+    async login () {
+      try {
+        this.errors = []
+        this.working = true
+        const response = await HTTP.post('auth/token/create/', {email: this.email, password: this.password})
+        this.authorized = true
+        // use response.data.auth_token to set authorization header in HTTP
+      } catch (e) {
+        console.log(e)
+        this.errors = e.response.data.non_field_errors
+      }
+      this.working = false
+    }
+  }
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped>
+  #login-button {
+    width: 100%;
+  }
+</style>
