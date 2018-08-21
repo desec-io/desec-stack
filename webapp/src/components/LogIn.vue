f31a00e5fbffa70a8c4bdc07450a6c676fba75f3
Nils Wisiol <info@nils-wisiol.de>
2018-08-21 15:37:00 +0200

    rewrote login based on sign up form

--- webapp/src/components/LogIn.vue
+++ webapp/src/components/LogIn.vue
@@ -1,50 +1,68 @@
 <template>
-  <v-card flat>
-    <v-form v-model="valid" v-on:submit.prevent="login">
-      <v-toolbar flat>
-        <v-toolbar-title>Log in</v-toolbar-title>
-      </v-toolbar>
-      <v-card-text>
-        <v-alert :value="errors && errors.length" type="error">
-          <div v-if="errors.length > 1">
-            <li v-for="error of errors" :key="error.message">
-              <b>{{ error.message }}</b>
-              {{ error }}
-            </li>
-          </div>
-          <div v-else>
-            {{ errors[0] }}
-          </div>
-        </v-alert>
-        <v-text-field
-          v-model="email"
-          :rules="email_rules"
-          label="E-mail"
-          required
-        ></v-text-field>
-        <v-text-field
-          v-model="password"
-          :append-icon="hide_password ? 'visibility' : 'visibility_off'"
-          @click:append="() => (hide_password = !hide_password)"
-          :type="hide_password ? 'password' : 'text'"
-          label="Enter your password"
-          counter
-          required
-        ></v-text-field>
-      </v-card-text>
-      <v-card-actions>
-        <v-btn
-          type="submit"
-          color="primary"
-          :disabled="!valid || working"
-          id="login-button"
-          :loading="working"
-        >
-          Log in
-        </v-btn>
-      </v-card-actions>
-    </v-form>
-  </v-card>
+  <v-flex style="max-width: 400px">
+    <v-container text-xs-center>
+      <v-form v-on:submit.prevent="login" v-model="valid">
+        <v-layout column>
+          <v-flex xs12 mb-5>
+            <img src="@/assets/logo.png">
+          </v-flex>
+          <v-flex x12>
+            <v-alert :value="errors && errors.length" type="error">
+              <div v-if="errors.length > 1">
+                <li v-for="error of errors" :key="error.message">
+                  <b>{{ error.message }}</b>
+                  {{ error }}
+                </li>
+              </div>
+              <div v-else>
+                {{ errors[0] }}
+              </div>
+            </v-alert>
+          </v-flex>
+          <v-flex x12>
+            <v-text-field
+              v-model="email"
+              label="E-Mail"
+              outline
+              required
+              :disabled="working"
+              :rules="email_rules"
+              :error-messages="email_errors"
+              @change="email_errors=[]"
+            ></v-text-field>
+          </v-flex>
+          <v-flex x12>
+            <v-text-field
+              v-model="password"
+              label="Password"
+              :append-icon="hide_password ? 'visibility' : 'visibility_off'"
+              @click:append="() => (hide_password = !hide_password)"
+              :type="hide_password ? 'password' : 'text'"
+              outline
+              required
+              :disabled="working"
+              :rules="password_rules"
+            ></v-text-field>
+          </v-flex>
+          <v-flex x12>
+            <v-btn
+              block
+              type="submit"
+              color="primary"
+              :disabled="!valid || working"
+              :loading="working"
+            >Log In</v-btn>
+          </v-flex>
+        </v-layout>
+      </v-form>
+      <v-layout column>
+        <v-flex xs12 mt-5>
+          No account yet?
+          <v-btn flat color="primary" :to="{name: 'SignUp'}">Sign up</v-btn>
+        </v-flex>
+      </v-layout>
+    </v-container>
+  </v-flex>
 </template>
 
 <script>
@@ -54,22 +72,23 @@ import router from '../router'
 export default {
   name: 'LogIn',
   data: () => ({
-    ack: false,
-    email: '',
-    email_rules: [
-      v => !!v || 'E-mail is required',
-      v => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'E-mail must be valid'
-    ],
-    hide_password: true,
-    password: '',
     valid: false,
     working: false,
+    email: '',
+    password: '',
+    terms: false,
+    email_rules: [ v => !!v || 'Please enter the email address associated with your account' ],
+    email_errors: [],
+    password_rules: [
+      v => !!v || 'Enter your password to log in'
+    ],
+    hide_password: true,
     errors: []
   }),
   methods: {
     async login () {
-      this.errors = []
       this.working = true
+      this.errors = []
       try {
         const response = await HTTP.post('auth/token/create/', {email: this.email, password: this.password})
         HTTP.defaults.headers.common['Authorization'] = 'Token ' + response.data.auth_token
@@ -78,22 +97,36 @@ export default {
         } else {
           router.replace({name: 'DomainList'})
         }
-      } catch (e) {
-        this.working = false
-        try {
-          this.errors = e.response.data.non_field_errors
-        } catch (ex) {
-          this.errors = [e, ex]
+      } catch (error) {
+        if (error.response) {
+          // The request was made and the server responded with a status code
+          // that falls out of the range of 2xx
+          if (error.response.status < 500) {
+            // 3xx or 4xx
+            if ('email' in error.response.data) {
+              this.email_errors = [error.response.data.email[0]]
+            } else if ('non_field_errors' in error.response.data) {
+              this.errors = error.response.data.non_field_errors
+            } else {
+              this.errors = error.response
+              console.log('Error', error)
+            }
+          } else {
+            // 5xx
+            this.errors = ['Something went wrong at the server, but we currently do not know why. The customer support was already notified.']
+            console.log('Error', error)
+          }
+        } else if (error.request) {
+          this.errors = ['Cannot contact our servers. Are you offline?']
+        } else {
+          this.errors = [error.message]
         }
       }
+      this.working = false
     }
   }
 }
 </script>
 
-<!-- Add "scoped" attribute to limit CSS to this component only -->
 <style scoped>
-  #login-button {
-    width: 100%;
-  }
 </style>
