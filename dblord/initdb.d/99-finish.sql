df15794833fe020d5ba8105924d66cc12d380223
Peter Thomassen <peter@desec.io>
2019-01-21 16:49:32 +0100

    chore(db): bump MariaDB version
    New version has ALTER USER statement, so we can clean up the account
    configuration a bit.

--- dblord/initdb.d/99-finish.sql
+++ dblord/initdb.d/99-finish.sql
@@ -1,5 +1,4 @@
 -- Narrow down root logins
+DROP USER 'root'@'%';
 INSTALL PLUGIN unix_socket SONAME 'auth_socket';
-DELETE FROM mysql.user WHERE User = 'root' AND Host != 'localhost';
-UPDATE mysql.user SET plugin = 'unix_socket' WHERE User = 'root' AND Host = 'localhost';
-FLUSH PRIVILEGES;
+ALTER USER 'root'@'localhost' IDENTIFIED VIA unix_socket;
