5488d3cd1b0c56dc5daf503a05c874ca48a59da4
Peter Thomassen <peter@desec.io>
2019-02-16 00:42:30 +0100

    fix(api): better adhere to DynDNS RA-API
    Rationale:
    - Status code: https://help.dyn.com/remote-access-api/guidelines/
    - Body: https://help.dyn.com/remote-access-api/return-codes/

--- api/desecapi/authentication.py
+++ api/desecapi/authentication.py
@@ -91,7 +91,7 @@ class URLParamAuthentication(BaseAuthentication):
         try:
             token = self.model.objects.get(key=key)
         except self.model.DoesNotExist:
-            raise exceptions.AuthenticationFailed('Invalid token')
+            raise exceptions.AuthenticationFailed('badauth')
 
         if not token.user.is_active:
             raise exceptions.AuthenticationFailed('User inactive or deleted')
