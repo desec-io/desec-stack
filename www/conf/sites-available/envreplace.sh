c67944d2a02f0d34d5d3cdbb02ea8420c9819ba1
Peter Thomassen <peter@desec.io>
2019-12-10 18:53:53 +0100

    feat(www): disable rate limit on api/ for e2e tests

--- www/conf/sites-available/envreplace.sh
+++ www/conf/sites-available/envreplace.sh
@@ -3,7 +3,13 @@
 # https://stackoverflow.com/questions/59895/can-a-bash-script-tell-which-directory-it-is-stored-in
 DIR="$( cd "$( dirname "${BASH_SOURCE[0]}" )" && pwd )"
 
-for file in $DIR/*.conf.var; do
+export PROD_ONLY=
+if [[ $DESECSTACK_E2E_TEST = "TRUE" ]]
+then
+    export PROD_ONLY='#'
+fi
+
+for file in $DIR/*.var; do
     # we only replace occurances of the variables specified below as first argument
-    (envsubst '$DESECSTACK_DOMAIN' | envsubst '$CERT_PATH') < $file > $DIR/`basename $file .var`
+    (envsubst '$DESECSTACK_DOMAIN' | envsubst '$CERT_PATH' | envsubst '$PROD_ONLY' ) < $file > $DIR/`basename $file .var`
 done
