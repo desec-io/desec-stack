930b5ecdeab65dbd36b27d76a99c96562bde69ef
Nils Wisiol <mail@nils-wisiol.de>
2020-04-26 12:55:41 +0200

    feat(nslord): build nslord from source, currently 4.2.x plus performance patch

--- nslord/entrypoint.sh
+++ nslord/entrypoint.sh
@@ -3,6 +3,6 @@
 host=dblord; port=3306; n=120; i=0; while ! (echo > /dev/tcp/$host/$port) 2> /dev/null; do [[ $i -eq $n ]] && >&2 echo "$host:$port not up after $n seconds, exiting" && exit 1; echo "waiting for $host:$port to come up"; sleep 1; i=$((i+1)); done
 
 # Manage credentials
-envsubst < /etc/powerdns/pdns.conf.var > /etc/powerdns/pdns.conf
+envsubst < /etc/powerdns/pdns.conf.var > /usr/local/etc/pdns.conf
 
 exec pdns_server --daemon=no
