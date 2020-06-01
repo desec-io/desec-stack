0d5ebe5ae5ffc461582d8df1ce05ec0b842c3189
Peter Thomassen <peter@desec.io>
2020-06-01 17:15:09 +0200

    refactor(): in chores, use method per chore

--- api/desecapi/management/commands/chores.py
+++ api/desecapi/management/commands/chores.py
@@ -2,16 +2,22 @@ from django.conf import settings
 from django.core.management import BaseCommand
 from django.utils import timezone
 
-from desecapi.models import Captcha, User
+from desecapi import models
 
 
 class Command(BaseCommand):
 
-    def handle(self, *args, **kwargs):
-        # delete expired captchas
-        Captcha.objects.filter(created__lt=timezone.now() - settings.CAPTCHA_VALIDITY_PERIOD).delete()
+    @staticmethod
+    def delete_expired_captchas():
+        models.Captcha.objects.filter(created__lt=timezone.now() - settings.CAPTCHA_VALIDITY_PERIOD).delete()
 
+    @staticmethod
+    def delete_never_activated_users():
         # delete inactive users whose activation link expired and who never logged in
         # (this will not delete users who have used their account and were later disabled)
-        User.objects.filter(is_active=False, last_login__exact=None,
+        models.User.objects.filter(is_active=False, last_login__exact=None,
                             created__lt=timezone.now() - settings.VALIDITY_PERIOD_VERIFICATION_SIGNATURE).delete()
+
+    def handle(self, *args, **kwargs):
+        self.delete_expired_captchas()
+        self.delete_never_activated_users()
