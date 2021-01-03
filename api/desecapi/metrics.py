d3e911c25a7f7c1d1d1de57da071a235d403ac24
Nils Wisiol <mail@nils-wisiol.de>
2021-01-03 17:31:58 +0100

    fix(api): adjust metrics.py formatting

--- api/desecapi/metrics.py
+++ api/desecapi/metrics.py
@@ -1,6 +1,5 @@
 from prometheus_client import Counter, Histogram
 
-
 metrics = {}
 
 
@@ -16,39 +15,32 @@ def set_histogram(name, *args, **kwargs):
     metrics[name] = Histogram(name, *args, **kwargs)
 
 
-#models.py metrics
-
+# models.py metrics
 set_counter('desecapi_captcha_content_created', 'number of times captcha content created', ['kind'])
 set_counter('desecapi_autodelegation_created', 'number of autodelegations added')
 set_counter('desecapi_autodelegation_deleted', 'number of autodelegations deleted')
-set_histogram('desecapi_messages_queued', 'number of emails queued', ['reason', 'user', 'lane'], buckets=[0, 1, float("inf")])
+set_histogram('desecapi_messages_queued', 'number of emails queued', ['reason', 'user', 'lane'],
+              buckets=[0, 1, float("inf")])
 
-#views.py metrics
-
+# views.py metrics
 set_counter('desecapi_dynDNS12_domain_not_found', 'number of times dynDNS12 domain is not found')
 
-#crypto.py metrics
-
+# crypto.py metrics
 set_counter('desecapi_key_encryption_success', 'number of times key encryption was successful', ['context'])
 set_counter('desecapi_key_decryption_success', 'number of times key decryption was successful', ['context'])
 
-#exception_handlers.py metrics
-
+# exception_handlers.py metrics
 set_counter('desecapi_database_unavailable', 'number of times database was unavailable')
 
-#pdns.py metrics
-
+# pdns.py metrics
 set_counter('desecapi_pdns_request_success', 'number of times pdns request was successful', ['method', 'status'])
 set_counter('desecapi_pdns_keys_fetched', 'number of times pdns keys were fetched')
 
-#pdns_change_tracker.py metrics
-
+# pdns_change_tracker.py metrics
 set_counter('desecapi_pdns_catalog_updated', 'number of times pdns catalog was updated successfully')
 
-#throttling.py metrics
-
+# throttling.py metrics
 set_counter('desecapi_throttle_failure', 'number of requests throttled', ['method', 'scope', 'user'])
 
-#serializers.py metrics
-
+# serializers.py metrics
 set_counter('desecapi_rrset_list_serializer', 'number of times RRsetListSerializer was initialized')
