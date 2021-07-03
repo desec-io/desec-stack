c2bb347b99a10ec15b562710496b18053d2292a9
Peter Thomassen <peter@desec.io>
2021-07-03 13:36:44 +0200

    fix(api): typo in tests

--- api/desecapi/tests/test_rrsets.py
+++ api/desecapi/tests/test_rrsets.py
@@ -193,7 +193,7 @@ class AuthenticatedRRSetTestCase(AuthenticatedRRSetBaseTestCase):
         response = self.client.post_rr_set(self.my_empty_domain.name, **data)
         self.assertContains(response, 'CNAME RRset cannot have empty subname', status_code=status.HTTP_400_BAD_REQUEST)
 
-    def test_create_my_rr_sets_cname_multple_records(self):
+    def test_create_my_rr_sets_cname_multiple_records(self):
         for records in (['foobar.com.', 'foobar.com.'], ['foobar.com.', 'foobar.org.']):
             data = {'subname': 'asdf', 'ttl': 3600, 'type': 'CNAME', 'records': records}
             response = self.client.post_rr_set(self.my_empty_domain.name, **data)
