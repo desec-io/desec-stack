336971aafeaa00244b50c66d09c55ecb8aa4f797
Peter Thomassen <peter@desec.io>
2021-07-13 14:45:49 +0200

    feat(api): return DS records as given by nslord's default-publish-cds

--- api/desecapi/pdns.py
+++ api/desecapi/pdns.py
@@ -93,16 +93,17 @@ def get_keys(domain):
     """
     Retrieves a dict representation of the DNSSEC key information
     """
-    def _filter_ds(key):
-        key['ds'] = [ds for ds in key['ds'] if int(ds.split()[2]) in [2, 4]]
-        return key
-
     r = _pdns_get(NSLORD, '/zones/%s/cryptokeys' % pdns_id(domain.name))
     metrics.get('desecapi_pdns_keys_fetched').inc()
-    keys = [{k: key[k] for k in ('dnskey', 'ds', 'flags', 'keytype')}
+    field_map = {
+        'dnskey': 'dnskey',
+        'cds': 'ds',
+        'flags': 'flags',
+        'keytype': 'keytype',
+    }
+    return [{v: key[k] for k, v in field_map.items()}
              for key in r.json()
              if key['published'] and key['keytype'] in ['csk', 'ksk']]
-    return list(map(_filter_ds, keys))
 
 
 def get_zone(domain):
--- test/e2e2/spec/test_api_domains.py
+++ test/e2e2/spec/test_api_domains.py
@@ -1,10 +1,16 @@
-from conftest import DeSECAPIV1Client, random_domainname
+from conftest import DeSECAPIV1Client, NSLordClient, random_domainname
 
 
 def test_create(api_user: DeSECAPIV1Client):
     assert len(api_user.domain_list()) == 0
     assert api_user.domain_create(random_domainname()).status_code == 201
     assert len(api_user.domain_list()) == 1
+
+
+def test_get(api_user_domain: DeSECAPIV1Client):
+    domain = api_user_domain.get(f"/domains/{api_user_domain.domain}/").json()
+    assert NSLordClient.query(api_user_domain.domain, 'CDS')[1] == set(domain['keys'][0]['ds'])
+    assert domain['name'] == api_user_domain.domain
 
 
 def test_destroy(api_user_domain: DeSECAPIV1Client):
