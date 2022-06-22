fe5b2ca1aa0d34d144f36e07001864b3f5d06a23
Nils Wisiol <mail@nils-wisiol.de>
2022-06-22 12:57:56 +0200

    chore(webapp): upgrade to vue-cli 5

--- webapp/vue.config.js
+++ webapp/vue.config.js
@@ -1,10 +1,7 @@
 module.exports = {
   configureWebpack: {
     devServer: {
-      disableHostCheck: true,
-      sockHost: 'desec.' + process.env.VUE_APP_DESECSTACK_DOMAIN,
-      public: 'https://desec.' + process.env.VUE_APP_DESECSTACK_DOMAIN + '/',
+      allowedHosts: 'all',
     },
   },
-  publicPath: '/',
 };
