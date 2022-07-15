e1838a50bf5cf32bff51743404db7353e93ba884
Peter Thomassen <peter@desec.io>
2022-07-15 20:02:09 +0200

    chore(webapp): bump dependencies

--- webapp/.eslintrc.js
+++ webapp/.eslintrc.js
@@ -12,7 +12,7 @@ module.exports = {
     'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off'
   },
   parserOptions: {
-    parser: 'babel-eslint'
+    parser: '@babel/eslint-parser'
   },
   overrides: [
     {
