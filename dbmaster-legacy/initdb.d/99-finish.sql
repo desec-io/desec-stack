b48cb6f6bbfa7a9e018a671bfa4e04902b2302db
Peter Thomassen <peter@desec.io>
2022-07-18 15:29:55 +0200

    BREAKING feat(nsmaster): detach from dbmaster-legacy
    When deployed with this commit as the HEAD, nsmaster will start the
    nameserver daemon (as usual), and reintroduce container restarting on
    error.
    
    After deploying, the legacy volume desec-stack_dbmaster_mysql may be
    deleted.

--- dbmaster-legacy/initdb.d/99-finish.sql
+++
@@ -1,4 +0,0 @@
--- Narrow down root logins
-DROP USER 'root'@'%';
-INSTALL PLUGIN unix_socket SONAME 'auth_socket';
-ALTER USER 'root'@'localhost' IDENTIFIED VIA unix_socket;
--- nsmaster/entrypoint.sh
+++ nsmaster/entrypoint.sh
@@ -8,7 +8,6 @@
 # TODO remove this workaround once the problem has been solved at its root
 iptables -t mangle -A OUTPUT -p udp -j TTL --ttl-set 64
 
-host=dbmaster-legacy; port=3306; n=120; i=0; while ! (echo > /dev/tcp/$host/$port) 2> /dev/null; do [[ $i -eq $n ]] && >&2 echo "$host:$port not up after $n seconds, exiting" && exit 1; echo "waiting for $host:$port to come up"; sleep 1; i=$((i+1)); done
 # wait for dbmaster database to come up
 until PGPASSWORD=$DESECSTACK_DBMASTER_PASSWORD_pdns psql -h dbmaster -U pdns -c '\q'; do
   >&2 echo "Postgres is unavailable - sleeping"
@@ -17,11 +16,6 @@ done
 
 # Manage credentials
 envsubst < /etc/powerdns/pdns.conf.var > /etc/powerdns/pdns.conf
-envsubst < /etc/powerdns/pdns-migrate.conf.var > /etc/powerdns/pdns-migrate.conf
-
-# Migrate
-pdnsutil --config-name migrate b2b-migrate gmysql gpgsql
-pdnsutil rectify-all-zones
 
 echo "Provisioning default TSIG key ..."
 pdnsutil import-tsig-key default hmac-sha256 "${DESECSTACK_NSMASTER_TSIGKEY}" > /dev/null
