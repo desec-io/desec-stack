desec-io/desec-stack

commit 1c6a6878708e43a5eba0bbf699219f33a4ad9c57

feat(api): look up nslord address only once

api/desecapi/pdns.py
@@ -82,6 +82,8 @@ _config = {
     },
 }
 
+_nslord_ip = socket.gethostbyname("nslord")
+
 
 def _pdns_request(
     method, *, server, path, data=None, accept="application/json", **kwargs
@@ -246,7 +248,7 @@ def create_zone_master(name):
         {
             "name": name,
             "kind": "SLAVE",
-            "masters": [socket.gethostbyname("nslord")],
+            "masters": [_nslord_ip],
             "master_tsig_key_ids": ["default"],
         },
     )

commit 81e7f452f6d37ba13b896531e3d81f52f6544326

fix(api): avoid nslord lookup on load (unbreaks Celery)
Regression introduced in 1c6a6878708e43a5eba0bbf699219f33a4ad9c57.

api/desecapi/pdns.py
@@ -1,6 +1,7 @@
 import json
 import re
 import socket
+from functools import cache
 from hashlib import sha1
 
 import requests
@@ -82,7 +83,10 @@ _config = {
     },
 }
 
-_nslord_ip = socket.gethostbyname("nslord")
+
+@cache
+def gethostbyname_cached(host):
+    return socket.gethostbyname(host)
 
 
 def _pdns_request(
@@ -248,7 +252,7 @@ def create_zone_master(name):
         {
             "name": name,
             "kind": "SLAVE",
-            "masters": [_nslord_ip],
+            "masters": [gethostbyname_cached("nslord")],
             "master_tsig_key_ids": ["default"],
         },
     )