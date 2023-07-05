desec-io/desec-stack

commit edb578bbd4a9320165ae84bc9aaa9bccb998b97d

migrate(webapp): from `@mdi/font` to `@mdi/js`
Use tree shakeable version instead of big webfont.

www/webapp/src/views/CrudList.vue
@@ -40,7 +40,7 @@
                 <v-text-field
                         v-model="search"
                         v-if="$vuetify.breakpoint.smAndUp"
-                        append-icon="mdi-magnify"
+                        :append-icon="mdiMagnify"
                         label="Search"
                         single-line
                         hide-details
@@ -62,12 +62,12 @@
                         depressed
                         :disabled="user.working"
                 >
-                  <v-icon>mdi-plus</v-icon>
+                  <v-icon>{{mdiPlus}}</v-icon>
                 </v-btn>
                 <template #extension v-if="$vuetify.breakpoint.xsOnly">
                   <v-text-field
                           v-model="search"
-                          append-icon="mdi-magnify"
+                          :append-icon="mdiMagnify"
                           label="Search"
                           single-line
                           hide-details
@@ -87,7 +87,7 @@
                         <span class="headline">{{ headlines.create }}</span>
                         <v-spacer />
                         <v-icon @click.stop="close">
-                          mdi-close
+                          {{ mdiClose }}
                         </v-icon>
                       </v-card-title>
                       <v-divider />
@@ -352,6 +352,7 @@ import GenericSwitchbox from '@/components/Field/GenericSwitchbox.vue';
 import TTL from '@/components/Field/TTL';
 import ErrorAlert from '@/components/ErrorAlert'
 import {useUserStore} from "@/store/user";
+import {mdiClose, mdiContentSaveEdit, mdiDelete, mdiMagnify, mdiPlus} from "@mdi/js";
 
 const filter = function (obj, predicate) {
   const result = {};
@@ -449,6 +450,11 @@ export default {
       }
     },
     handleRowClick: () => {},
+    mdiClose,
+    mdiDelete,
+    mdiContentSaveEdit,
+    mdiMagnify,
+    mdiPlus,
   }},
   computed: {
     actions: () => {},
@@ -458,13 +464,13 @@ export default {
         'save': {
           go: d => this.save(d),
           if: this.updatable,
-          icon: 'mdi-content-save-edit',
+          icon: mdiContentSaveEdit,
           tooltip: 'Save',
         },
         'delete': {
           go: d => this.destroyAsk(d),
           if: this.destroyable,
-          icon: 'mdi-delete',
+          icon: mdiDelete,
           tooltip: 'Delete',
         },
       }

www/webapp/src/App.vue
@@ -70,7 +70,7 @@
                       v-on="on"
               >
                 more
-                <v-icon right>mdi-menu-down</v-icon>
+                <v-icon right>{{mdiMenuDown}}</v-icon>
               </v-btn>
             </template>
 
@@ -153,7 +153,7 @@
         <div>
           <p>
             Please <router-link :to="{name: 'donate'}">donate</router-link>!
-            <v-icon color="red" class="text--darken-2" dense>mdi-heart</v-icon>
+            <v-icon color="red" class="text--darken-2" dense>{{mdiHeart}}</v-icon>
           </p>
           <p>
             European Bank Account:<br>
@@ -182,6 +182,17 @@
 import router from '@/router';
 import {logout} from '@/utils';
 import {useUserStore} from "@/store/user";
+import {
+    mdiBookOpenPageVariant,
+    mdiForumOutline,
+    mdiGiftOutline,
+    mdiHeart,
+    mdiHome,
+    mdiLockReset,
+    mdiMenuDown,
+    mdiRoadVariant,
+    mdiUmbrella
+} from "@mdi/js";
 
 export default {
   name: 'App',
@@ -189,42 +200,44 @@ export default {
     user: useUserStore(),
     drawer: false,
     email: process.env.VUE_APP_EMAIL,
+    mdiHeart,
+    mdiMenuDown,
     menu: {
       'home': {
         'name': 'home',
-        'icon': 'mdi-home',
+        'icon': mdiHome,
         'text': 'Home',
       },
       'docs': {
         'name': 'docs',
-        'icon': 'mdi-book-open-page-variant',
+        'icon': mdiBookOpenPageVariant,
         'text': 'Docs',
       },
       'roadmap': {
         'name': 'roadmap',
-        'icon': 'mdi-road-variant',
+        'icon': mdiRoadVariant,
         'text': 'Roadmap',
       },
       'talk': {
         'name': 'talk',
-        'icon': 'mdi-forum-outline',
+        'icon': mdiForumOutline,
         'text': 'Talk',
       },
       'donate': {
         'name': 'donate',
-        'icon': 'mdi-gift-outline',
+        'icon': mdiGiftOutline,
         'text': 'Donate',
-        'post_icon': 'mdi-heart',
+        'post_icon': mdiHeart,
         'post_icon_color': 'red',
       },
       'about': {
         'name': 'about',
-        'icon': 'mdi-umbrella',
+        'icon': mdiUmbrella,
         'text': 'About',
       },
       'reset-password': {
         'name': 'reset-password',
-        'icon': 'mdi-lock-reset',
+        'icon': mdiLockReset,
         'text': 'Reset Account Password',
       },
     },

www/webapp/src/components/DonateDirectDebitForm.vue
@@ -35,7 +35,7 @@
       <v-text-field
               v-model="name"
               label="Full Name of the Account Holder"
-              prepend-icon="mdi-account"
+              :prepend-icon="mdiAccount"
               outline
               required
               :disabled="working"
@@ -46,7 +46,7 @@
       <v-text-field
               v-model="iban"
               label="IBAN"
-              prepend-icon="mdi-bank"
+              :prepend-icon="mdiBank"
               outline
               required
               :disabled="working"
@@ -58,7 +58,7 @@
       <v-text-field
               v-model="amount"
               label="Amount in Euros"
-              prepend-icon="mdi-cash-100"
+              :prepend-icon="mdiCash100"
               outline
               required
               :disabled="working"
@@ -69,7 +69,7 @@
       <v-text-field
               v-model="message"
               label="Message (optional)"
-              prepend-icon="mdi-message-text-outline"
+              :prepend-icon="mdiMessageTextOutline"
               outline
               :disabled="working"
               validate-on-blur
@@ -78,7 +78,7 @@
       <v-text-field
               v-model="email"
               label="Email Address (optional)"
-              prepend-icon="mdi-email"
+              :prepend-icon="mdiEmail"
               outline
               :disabled="working"
               :rules="email_rules"
@@ -103,6 +103,7 @@
   import {email_pattern} from '@/validation';
   import {digestError} from '@/utils';
   import ErrorAlert from '@/components/ErrorAlert';
+  import {mdiAccount, mdiBank, mdiCash100, mdiEmail, mdiMessageTextOutline} from "@mdi/js";
 
   const HTTP = axios.create({
     baseURL: '/api/v1/',
@@ -120,6 +121,13 @@
       working: false,
       done: false,
       errors: [],
+
+      /* icons */
+      mdiAccount,
+      mdiBank,
+      mdiCash100,
+      mdiMessageTextOutline,
+      mdiEmail,
 
       /* from env */
       creditorid: process.env.VUE_APP_DESECSTACK_API_SEPA_CREDITOR_ID,

www/webapp/src/components/Field/RecordList.vue
@@ -7,7 +7,7 @@
             :content="item"
             :error-messages="errorMessages[index] ? errorMessages[index].content : []"
             :hide-label="index > 0"
-            :append-icon="value.length > 1 ? 'mdi-close' : ''"
+            :append-icon="value.length > 1 ? mdiClose : ''"
             :disabled="disabled"
             :readonly="readonly"
             :required="required"
@@ -24,7 +24,7 @@
             small
             text
             v-if="!readonly && !disabled"
-    ><v-icon>mdi-plus</v-icon> add another value</v-btn>
+    ><v-icon>{{mdiPlus}}</v-icon> add another value</v-btn>
     <!--div><code style="white-space: normal">{{ value }}</code></div-->
   </div>
 </template>
@@ -50,6 +50,7 @@ import RecordSVCB from './Record/SVCB.vue';
 import RecordTLSA from './Record/TLSA.vue';
 import RecordTXT from './Record/TXT.vue';
 import RecordSubnet from './Record/Subnet.vue';
+import {mdiClose, mdiPlus} from "@mdi/js";
 
 export default {
   name: 'RecordList',
@@ -104,6 +105,8 @@ export default {
   data: function () {
     const self = this;
     return {
+      mdiClose,
+      mdiPlus,
       types: [
         'A',
         'AAAA',

www/webapp/src/plugins/vuetify.js
@@ -7,6 +7,9 @@ Vue.use(Vuetify);
 
 
 export default new Vuetify({
+  icons: {
+    iconfont: 'mdiSvg', // 'mdi' || 'mdiSvg' || 'md' || 'fa' || 'fa4' || 'faSvg'
+  },
   theme: {
     themes: {
       light: {

www/webapp/src/views/Console/DomainSetupDialog.vue
@@ -13,7 +13,7 @@
         </div>
         <v-spacer/>
         <v-icon @click.stop="close">
-          mdi-close
+          {{ mdiClose }}
         </v-icon>
       </v-card-title>
       <v-divider/>
@@ -35,6 +35,7 @@
 
 <script>
 import DomainSetup from "@/views/DomainSetup";
+import {mdiClose} from "@mdi/js";
 
 export default {
   name: 'DomainSetupDialog',
@@ -50,6 +51,7 @@ export default {
     },
   },
   data: () => ({
+    mdiClose,
     value: {
       type: Boolean,
       default: true,

www/webapp/src/views/Console/TOTPVerifyDialog.vue
@@ -14,7 +14,7 @@
           </div>
           <v-spacer/>
           <v-icon @click.stop="close">
-            mdi-close
+            {{mdiClose}}
           </v-icon>
         </v-card-title>
         <v-divider/>
@@ -29,14 +29,14 @@
 
         <v-card-text v-if="!!successDetail" class="text-center">
           <p class="mt-2">
-            <v-icon>mdi-check</v-icon>
+            <v-icon>{{mdiCheck}}</v-icon>
             Great! Continue to <router-link :to="{name: 'login'}">log in</router-link>.
           </p>
         </v-card-text>
 
         <v-card-text v-if="!!data && !successDetail" class="text-center">
           <p class="mt-2">
-            <v-icon>mdi-numeric-1-circle</v-icon>
+            <v-icon>{{mdiNumeric1Circle}}</v-icon>
             Please scan the following QR code with an authenticator app (e.g. Google Authenticator).<br />
             <strong>This code is only displayed once.</strong>
           </p>
@@ -44,7 +44,7 @@
             <qrcode-vue :value="data.uri" size="300" level="H"/>
           </div>
           <p class="mt-6">
-            <v-icon>mdi-numeric-2-circle</v-icon>
+            <v-icon>{{mdiNumeric2Circle}}</v-icon>
             Enter the code displayed in the authenticator app to confirm and activate the token:
           </p>
 
@@ -93,6 +93,7 @@
 import {digestError, HTTP, withWorking} from '@/utils'
 import ErrorAlert from "../../components/ErrorAlert";
 import QrcodeVue from '../../modules/qrcode.vue/dist/qrcode.vue.esm'
+import {mdiCheck, mdiClose, mdiNumeric1Circle, mdiNumeric2Circle} from "@mdi/js";
 
 export default {
   name: 'TOTPVerifyDialog',
@@ -120,6 +121,10 @@ export default {
     errors: [],
     working: false,
     show: true,
+    mdiCheck,
+    mdiClose,
+    mdiNumeric1Circle,
+    mdiNumeric2Circle,
   }),
   methods: {
     close() {

www/webapp/src/views/CrudListDomain.vue
@@ -2,6 +2,7 @@
 import { HTTP, withWorking } from '@/utils';
 import CrudList from './CrudList';
 import DomainSetupDialog from '@/views/Console/DomainSetupDialog';
+import {mdiDownload, mdiInformation} from "@mdi/js";
 
 export default {
   name: 'CrudListDomain',
@@ -120,12 +121,12 @@ export default {
       return {
         'info': {
           go: d => this.showDomainInfo(d),
-          icon: 'mdi-information',
+          icon: mdiInformation,
           tooltip: 'Setup instructions',
         },
         'export': {
           go: d => this.exportDomain(d),
-          icon: 'mdi-download',
+          icon: mdiDownload,
           if: this.showAdvanced,
           tooltip: 'Export (zonefile format)',
         },

commit d97827bb302dd7ec988e6027a29b50c9ba55247f

fix(webapp): regression from edb578bbd4a9320165ae84bc9aaa9bccb998b97d

www/webapp/src/views/CrudList.vue
@@ -223,6 +223,7 @@
                               v-bind="attrs"
                               v-on="on"
                               :disabled="user.working || itemIsReadOnly(itemFieldProps.item)"
+                              :class="'button-' + key"
                               color="grey"
                               icon
                               @click.stop="action.go(itemFieldProps.item, $event)"
@@ -446,7 +447,7 @@ export default {
       if (e.keyCode === 13) {
         // Submit
         document.activeElement.blur();
-        e.target.closest('tr').querySelector('.mdi-content-save-edit').closest('button').click();
+        e.target.closest('tr').querySelector('.button-save').click();
       }
     },
     handleRowClick: () => {},
@@ -710,7 +711,7 @@ export default {
   @keyframes successFade {
     from { background-color: forestgreen; }
   }
-  >>> tr.orange .mdi-content-save-edit, >>> tr.red .mdi-content-save-edit {
+  >>> tr.orange .button-save .v-icon, >>> tr.red .button-save .v-icon {
     color: forestgreen;
   }
   >>> tr:focus-within :focus {

www/webapp/src/views/CrudListRecord.vue
@@ -143,7 +143,7 @@ export default {
     @keyframes successFade {
         from { background-color: forestgreen; }
     }
-    >>> tr:focus-within .mdi-content-save-edit {
+    >>> tr:focus-within .button-save .v-icon {
         color: forestgreen;
     }
     >>> tr:focus-within :focus {