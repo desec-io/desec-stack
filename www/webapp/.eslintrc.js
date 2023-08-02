desec-io/desec-stack

commit 4644d36394f963d672e42844b5fd59164c0ff7ab

feat(webapp): use `eslint` as standalone
Remove dependency on vue-cli to enable migration to vite.

www/webapp/.eslintrc.js
@@ -1,8 +1,12 @@
+/* eslint-env node */
+
 /** @type {import('eslint').Linter.Config} */
 module.exports = {
   root: true,
   env: {
-    node: true
+    browser: true,
+    node: true, // Can be removed after migration to vite.
+    es2022: true,
   },
   extends: [
     'plugin:vue/essential',
@@ -24,17 +28,4 @@ module.exports = {
     'vue/no-deprecated-html-element-is': 'warn', // Preparation for vue3
   },
   ignorePatterns: ['**/src/modules/**/*'],
-  parserOptions: {
-    parser: '@babel/eslint-parser'
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)'
-      ],
-      env: {
-        mocha: true
-      }
-    }
-  ]
 }

commit 8e9f7b9631b7ba51cab1793bd00fe1b0a5ed89ac

fix(webapp): `eslint` regression by #770
Not all files are processed by `eslint`.

www/webapp/.eslintrc.js
@@ -6,7 +6,11 @@ module.exports = {
   env: {
     browser: true,
     node: true, // Can be removed after migration to vite.
-    es2022: true,
+    es2024: true,
+  },
+  parserOptions: {
+    ecmaVersion: 'latest',
+    sourceType: 'module',
   },
   extends: [
     'plugin:vue/essential',