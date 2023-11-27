14427d51ed2833527e167b0374cbe7598914dd4b
Peter Thomassen <peter@desec.io>
2023-11-27 16:00:06 +0100

    chore(api); upgrade to Django 5.0

--- api/api/settings.py
+++ api/api/settings.py
@@ -83,8 +83,6 @@ CACHES = {
 # This is necessary because the default is America/Chicago
 TIME_ZONE = "UTC"
 
-USE_TZ = True
-
 REST_FRAMEWORK = {
     "DEFAULT_RENDERER_CLASSES": ("rest_framework.renderers.JSONRenderer",),
     "DEFAULT_PARSER_CLASSES": ("rest_framework.parsers.JSONParser",),
