d352c3707d63006fd50bcfe8030c4b66052169e8
Peter Thomassen <peter@desec.io>
2025-02-11 12:25:23 +0100

    fix(api): remove unused error handling from MySQL times

--- api/desecapi/exception_handlers.py
+++ api/desecapi/exception_handlers.py
@@ -1,6 +1,6 @@
 import logging
 
-from django.db.utils import IntegrityError, OperationalError
+from django.db.utils import IntegrityError
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.views import exception_handler as drf_exception_handler
@@ -34,31 +34,6 @@ def exception_handler(exc, context):
             {"detail": "Internal Server Error. We're on it!"},
             status=status.HTTP_500_INTERNAL_SERVER_ERROR,
         )
-
-    def _503():
-        _log()
-        return Response(
-            {"detail": "Please try again later."},
-            status=status.HTTP_503_SERVICE_UNAVAILABLE,
-        )
-
-    # Catch DB OperationalError and log an extra error for additional context
-    if (
-        isinstance(exc, OperationalError)
-        and isinstance(exc.args, (list, dict, tuple))
-        and exc.args
-        and exc.args[0]
-        in (
-            2002,  # Connection refused (Socket)
-            2003,  # Connection refused (TCP)
-            2005,  # Unresolved host name
-            2007,  # Server protocol mismatch
-            2009,  # Wrong host info
-            2026,  # SSL connection error
-        )
-    ):
-        metrics.get("desecapi_database_unavailable").inc()
-        return _503()
 
     handlers = {
         IntegrityError: _409,
