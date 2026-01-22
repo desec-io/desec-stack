9254eee577b601ed5efa44f4adb5a82d56e09a56
Matthew Donoughe <mdonoughe@pm.me>
2026-01-22 20:13:55 +0900

    fix(webapp): allow IDN TLDs in CNAME targets

--- www/webapp/src/components/Field/RecordCNAME.vue
+++ www/webapp/src/components/Field/RecordCNAME.vue
@@ -2,7 +2,7 @@
 import { helpers } from 'vuelidate/lib/validators';
 import RecordItem from './RecordItem.vue';
 
-const domain = helpers.regex('domain', /^(([a-zA-Z0-9_\\-]+\.)+[a-zA-Z]{2,})[.]?$/);
+const domain = helpers.regex('domain', /^(([a-zA-Z0-9_\\-]+\.)+[a-zA-Z0-9\\-]{2,})[.]?$/);
 const trailingDot = helpers.regex('trailingDot', /[.]$/);
 
 export default {
